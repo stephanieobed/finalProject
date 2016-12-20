2d0a0e4bd9c1348007df2556ce5899cd55fc884f
stephanieobed <stephanieobed@users.noreply.github.com>
2016-12-19 22:19:54 -0500

    Add files via upload
    how do u fade tho

--- javascript/main-semantic.js
+++ javascript/main-semantic.js
@@ -1,4 +1,9 @@
-// Google Maps API
+// Semantic
+
+$('.trigger').transition('fade left')
+;
+
+// Google Maps API + OSM layer
 
 var map;
       function initMap() {
@@ -30,37 +35,37 @@ var map;
             }));
       }
 
-// // Flickr API 
+// Flickr API 
 
-// var req = new XMLHttpRequest();
-// req.open(
-//     "GET",
-//     "https://api.flickr.com/services/rest/?" +
-//         "method=flickr.photos.search&" +
-//         "api_key=75f73b938c8b88fce741814c87fd6465&" +
-//         "text=forest&hudson-valley&hiking" +
-//         "safe_search=1&" +  // 1 is "safe"
-//         "content_type=1&" +  // 1 is "photos only"
-//         "sort=relevance&" +  // another good one is "interestingness-desc"
-//         "per_page=20&format=json",
-//     true);
-// req.onload = showPhotos;
-// req.send(null);
+var req = new XMLHttpRequest();
+req.open(
+    "GET",
+    "https://api.flickr.com/services/rest/?" +
+        "method=flickr.photos.search&" +
+        "api_key=75f73b938c8b88fce741814c87fd6465&" +
+        "text=forest&hudson-valley&hiking" +
+        "safe_search=1&" +  // 1 is "safe"
+        "content_type=1&" +  // 1 is "photos only"
+        "sort=relevance&" +  // another good one is "interestingness-desc"
+        "per_page=20&format=json",
+    true);
+req.onload = showPhotos;
+req.send(null);
 
-// function showPhotos(data) {
-//     console.log(this.responseText)
-// //   var photos = req.responseXML.getElementsByTagName("photo");
-// //   console.log(data)
-// //   for (var i = 0, photo; photo = photos[i]; i++) {
-// //       console.log(photo)
-// //   }
-// }
+function showPhotos(data) {
+    console.log(this.responseText)
+//   var photos = req.responseXML.getElementsByTagName("photo");
+//   console.log(data)
+//   for (var i = 0, photo; photo = photos[i]; i++) {
+//       console.log(photo)
+//   }
+}
 
-// function constructImageURL(photo) {
-//   return "http://farm" + photo.farm +
-//       ".static.flickr.com/" + photo.server +
-//       "/" + photo.id +
-//       "_" + photo.secret +
-//       "_m.jpg";
-// }
+function constructImageURL(photo) {
+  return "http://farm" + photo.farm +
+      ".static.flickr.com/" + photo.server +
+      "/" + photo.id +
+      "_" + photo.secret +
+      "_m.jpg";
+}
 
